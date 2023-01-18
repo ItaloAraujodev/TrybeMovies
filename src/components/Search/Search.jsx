90807f9b72aea114270fc6e7b27e63500ff65661
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-01-18 17:09:10 -0300

    Resolvendo erro de rota

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import cartContext from '../../context/Context';
 import "./search.css"
 
@@ -7,6 +7,12 @@ const Search = () => {
 
   const imagensMovides = `https://image.tmdb.org/t/p/w500`;
   const { searchResult, setSearchResult } = useContext(cartContext)
+  const navigate = useNavigate();
+
+  function newRouter (id) {
+    navigate(`/movie/${id}`)
+  }
+
 
   return (
     <div className='container-fluid'>
@@ -18,20 +24,19 @@ const Search = () => {
         <div className='search'>
           {console.log(searchResult)}
           {searchResult.map((item, index) => (
-            <Link key={index} to={`movie/${item.id}`} style={{ textDecoration: 'none' }}>
-              <div className='container-card'>
-                <div className='card-search'>
-                  <div>
-                    <img src={`${imagensMovides}${item.poster_path}`} alt="" />
-                  </div>
-                  <div className='itens'>
-                    <h3 style={{ color: 'white' }}>{item.title}</h3>
-                    <p style={{ color: '#a9a9ab' }}>{item.release_date}</p>
-                    <p style={{ color: 'white' }}>{item.overview}</p>
-                  </div>
+            <div key={index} className='container-card' onClick={() => navigate(`/movie/${item.id}`)}>
+              <div className='card-search'>
+                <div>
+                  <img src={`${imagensMovides}${item.poster_path}`} alt="" />
+                </div>
+                <div className='itens'>
+                  <h3 style={{ color: 'white' }}>{item.title}</h3>
+                  <p style={{ color: '#a9a9ab' }}>{item.release_date}</p>
+                  <p style={{ color: 'white' }}>{item.overview}</p>
                 </div>
               </div>
-            </Link>
+            </div>
+
           ))}
         </div>
       </div>
